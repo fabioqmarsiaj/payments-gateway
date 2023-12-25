89598278a0c06f41fdf5738b5cdb3b06f1bf3746
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-25 15:49:58 -0300

    update to use go 1.21; credit_card domain and unit test

---
+++ golang/domain/entity/credit_card_test.go
@@ -0,0 +1,13 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreditCardNumber(t *testing.T) {
+	_, err := NewCreditCard("40000000000000000", "Credit card holder name", 12, 2024, 123)
+	assert.Equal(t, "invalid credit card number", err.Error())
+
+}

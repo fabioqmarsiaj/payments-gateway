7e7e2c342cbc9f712b155678bd8ced7848d5679e
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-25 15:55:35 -0300

    Valid credit card number test

--- golang/domain/entity/credit_card_test.go
+++ golang/domain/entity/credit_card_test.go
@@ -10,4 +10,7 @@ func TestCreditCardNumber(t *testing.T) {
 	_, err := NewCreditCard("40000000000000000", "Credit card holder name", 12, 2024, 123)
 	assert.Equal(t, "invalid credit card number", err.Error())
 
+	_, err = NewCreditCard("4193523830170205", "Credit card holder name", 12, 2024, 123)
+	assert.Nil(t, err)
+
 }
